be02ff0922cdd779cca5c501afdc78aebbfe2abc
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-02-27 21:16:14 -0300

    feat: add loading on home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 async function getFeaturedProducts(): Promise<Product[]> {
   const response = await api('/products/featured', {
     next: {
-      revalidate: 60 * 60, // 1 hour
+      revalidate: 1, // 1 hour
     },
   })
   const products = await response.json()
